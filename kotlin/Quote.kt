de5186d8957454b12b91315e5a87750bcd5635b6
Ömer - Tasarik <omercanakkaleli@gmail.com>
2026-09-03 17:58:58 +0300

    feat: add kotlin mini app

---
+++ kotlin/Quote.kt
@@ -0,0 +1,4 @@
+fun main() {
+ val quotes=listOf("Make it work, then make it beautiful.","Small steps ship big ideas.","Clarity is a feature.","Build, learn, repeat.")
+ println("✦ "+quotes.random())
+}
